4ee94fae0d2f654a0343b621c58761745d4d9b42
Yash Garg <thisisyashgarg@gmail.com>
2023-02-25 22:46:36 +0530

    port changed, some code added

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors";
-const app = express();
-const port = 4000;
+import bodyParser from "body-parser";
 
-app.get("/", async (req, res) => {
+const app = express();
+const port = 4001;
+app.use(express.json());
+
+app.get("/", cors(), async (req, res) => {
   const data = await fetch(
     "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING",
     {
@@ -20,8 +23,29 @@ app.get("/", async (req, res) => {
   const json = await data.json();
   res.send(json);
   console.log("data sent");
-  console.log(json);
 });
+
+app.post("/menu", (req, res) => {
+  console.log("menu called");
+  console.log(req.body);
+  //   const data = await fetch(
+  //     "https://www.swiggy.com/dapi/menu/v4/full?lat=12.9351929&lng=77.62448069999999&menuId=",
+  //     {
+  //       headers: {
+  //         Cookie:
+  //           "__SW=ZBO_ZDzTjhBLk0RXd8SfI2xexeHGhzZP; _device_id=27dec6a8-ce76-b6a7-3592-e2822125a716; _guest_tid=fd7cc029-f677-4e26-adcd-5ddcff1e5d1d; _sid=5lw3a733-6deb-4049-ae73-21a645adf9e9",
+  //         "User-Agent": "PostmanRuntime/7.30.0",
+  //         Accept: "*/*",
+  //         "Accept-Encoding": "gzip, deflate, br",
+  //         Connection: "keep-alive",
+  //       },
+  //     }
+  //   );
+  //   const json = await data.json();
+  //   res.send(json);
+  //   console.log("data sent");
+});
+
 app.listen(port, () => {
   console.log(`listening to ${port}`);
 });
